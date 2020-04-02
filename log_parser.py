8534420ef463e3b5d128f135a1c9491097e8ae08
Matt Hambro <matthew.hambro@gmail.com>
2020-04-02 10:22:12 -0700

    Get country, region

--- log_parser.py
+++ log_parser.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 
 import os
+import requests
 
 cwd = os.getcwd()
 file_path = cwd + "/" + "example.log"
+ip_location_base_url = "https://ipapi.co/"
+ip_location_suffix = "/json"
 
 with open(file_path, "r") as file:
 	for line in file:
 		ip = line.split(" ")[0]
 		user_agent = line.rsplit("\"")[-2]
-		print(ip)
-		print(user_agent)
+		#print(ip)
+		#print(user_agent)
+		ip_location_url = ip_location_base_url + ip + ip_location_suffix
+		ip_location_response = requests.get(ip_location_url).json() 
+		country = ip_location_response['country_name']
+		region = ip_location_response['region']
+		print(country, region)
+		
+
 
