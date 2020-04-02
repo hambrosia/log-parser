f1f88b580e24c6d2c85504a1b686c99d385c8d26
Matt Hambro <matthew.hambro@gmail.com>
2020-04-02 13:21:39 -0700

    Write CSV

--- log_parser.py
+++ log_parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+import csv
 import os
 import requests
 
 cwd = os.getcwd()
-file_path = cwd + "/" + "example.log"
+target_path = cwd + "/" + "example.log"
 
 userstack_key = os.getenv('USERSTACK_KEY')
 userstack_url = "http://api.userstack.com/detect"
@@ -12,11 +13,17 @@ userstack_url = "http://api.userstack.com/detect"
 ip_location_base_url = "https://ipapi.co/"
 ip_location_suffix = "/json"
 
+csv_name = "location_to_device.csv"
+output_path = cwd + "/" + csv_name
+column_descriptions = ["Country", "State", "Device", "Browser"]
+
+location_device_to_write = []
+
 def get_location(url: str) -> {}:
-	ip_location_response = requests.get(url).json() 
+	ip_location_response = requests.get(url).json()
 	country = ip_location_response['country_name']
 	region = ip_location_response['region']
-	return {"country" : country, "region": region}
+	return {'country' : country, 'region': region}
 
 def get_device_browser(ua: str) -> {}:
 	params = {
@@ -26,15 +33,22 @@ def get_device_browser(ua: str) -> {}:
 	useragent_response = requests.get(userstack_url, params).json()
 	device = useragent_response['device']['type']
 	browser = useragent_response['browser']['name']
-	return {"device" : device, "browser": browser}
- 	
-with open(file_path, "r") as file:
-	for line in file:
+	return {'device' : device, 'browser': browser}
+
+with open(target_path, "r") as target, open(output_path, "w", newline = "") as output:
+	writer = csv.writer(output)
+ 	# writer.writerow(column_descriptions)
+
+	for line in target:
 		ip = line.split(" ")[0]
 		user_agent = line.rsplit("\"")[-2]
+
 		ip_location_url = ip_location_base_url + ip + ip_location_suffix
 		location = get_location(ip_location_url)
 		print(str(location))
+
 		device_browser = get_device_browser(user_agent)
-		print(str(device_browser))	
+		print(str(device_browser))
+
+		writer.writerow([location['country'], location['region'], device_browser['device'], device_browser['browser']])
 
